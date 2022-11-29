9273cdc7689fd5b029134f3a40952d6fc52fcda4
Spikesito <emileseguret@icloud.com>
2022-11-29 10:47:15 +0100

    conflict chacal

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
+
+use Twig\Profiler\Dumper\HtmlDumper;
+
 include './View/header.php';
 include './View/body.php';
 include './View/footer.php';
 ?>
+
+
+ <!-- Routing  -->
+<!-- <html>
+    <form action="post_form.php" method="POST">
+        <input type="text" name="firstname"></input>
+        <input type="submit" value="Envoyer"></input>
+    </form>
+</html> -->
+
+<html>
+    <form action="post_form.php" method="GET">
+        <input type="text" name="firstname"></input>
+        <input type="submit" value="Envoyer"></input>
+    </form>
+</html>
+
